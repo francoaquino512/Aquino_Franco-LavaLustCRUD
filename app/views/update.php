e9d19a2079a708d9dec08087d7b7e02511f6925e
Franco Aquino <aquinofranco132@gmail.com>
2025-09-13 13:28:25 +0800

    First commit

--- app/views/update.php
+++ app/views/update.php
@@ -10,11 +10,11 @@
     <h1>💥🎯Valorant Update View🎯💥</h1>
     <form action="<?=site_url('user/update/'.$user['id'])?>" method="post">
         <label for="username">Username</label><br>
-        <input type="text" name="username" id="username" value="<?=html_escape($user['username']);?>"><br>
+        <input type="text" name="username" id="username" required value="<?=html_escape($user['username']);?>"><br>
         <label for="email">Email</label><br>
-        <input type="email" name="email" id="email" value="<?=html_escape($user['email']);?>"><br>
+        <input type="email" name="email" id="email" required value="<?=html_escape($user['email']);?>"><br>
         <label for="role">Role</label><br>
-        <input type="text" name="role" id="role" value="<?=html_escape($user['role']);?>"><br>
+        <input type="text" name="role" id="role" required value="<?=html_escape($user['role']);?>"><br>
         <button type="submit">Submit</button>
     </form>
 </body>
