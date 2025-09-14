77fc11f1be7133c076644467cf796b4fd1aba4df
Franco Aquino <aquinofranco132@gmail.com>
2025-09-14 17:15:20 +0800

    First commit

--- app/models/UserModel.php
+++ app/models/UserModel.php
@@ -2,7 +2,7 @@
 defined('PREVENT_DIRECT_ACCESS') OR exit('No direct script access allowed');
 
 class UserModel extends Model {
-    protected $table ='mockdata';
+    protected $table ='users';
     protected $primary_key = 'id';
 	public function __construct() {
 		parent::__construct();
